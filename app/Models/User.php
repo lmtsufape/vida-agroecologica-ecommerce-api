c2174d887ec02c2263009c80d9046dfeeeb8a8d8
Ivan do Nascimento <nascimentodoivan@gmail.com>
2023-01-28 14:24:02 -0300

    Modelo e migração do User

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'apelido',
+        'telefone',
+        'cpf'
     ];
 
     /**
@@ -41,4 +44,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function papel()
+    {
+        return $this->morphTo();
+    }
+    public function endereco()
+    {
+        return $this->morphOne(Endereco::class, "origem");
+    }
 }
