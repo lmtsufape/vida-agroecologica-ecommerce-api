03ddff14e5538ddb45c094e683e2b8b399dbd00b
IvandoNascimento <nascimentodoivan@gmail.com>
2023-02-15 20:29:28 -0300

    Modelo e migração de produto

---
+++ app/Models/Produto.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'descrição',
+        'estoque',
+        'preço',
+        'tipo_unidade',
+        'custo'
+    ];
+    
+}
