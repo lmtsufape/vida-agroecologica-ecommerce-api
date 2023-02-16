cbfdf8a02f3d01a36d33a692be17bff3ac9224a8
IvandoNascimento <nascimentodoivan@gmail.com>
2023-02-16 17:50:27 -0300

    Adicionado nulo ao campo de apelido do usuário

--- database/migrations/2023_02_12_000000_create_users_table.php
+++ database/migrations/2023_02_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('apelido');
+            $table->string('apelido')->nullable(true);
             $table->string('telefone');
             $table->string('cpf')->unique()->nullable(true);
             $table->string('cnpj')->unique()->nullable(true);
