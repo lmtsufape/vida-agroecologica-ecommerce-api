4e27f1ff6539b421cd0ebb7af98ae98b92a4c4f1
IvandoNascimento <nascimentodoivan@gmail.com>
2023-02-16 17:48:21 -0300

    Adicionado campo de nome a categoria

--- database/migrations/2023_02_06_185004_create_categorias_table.php
+++ database/migrations/2023_02_06_185004_create_categorias_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome');
+
         });
     }
 
