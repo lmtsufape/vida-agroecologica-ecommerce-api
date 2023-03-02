325b197099fea34b106e257adf21434778b68fa9
Ivan do Nascimento <nascimentodoivan@gmail.com>
2023-03-02 13:22:58 -0300

    Criação modelo, migração e controlador(wip) da sacola

---
+++ app/Http/Controllers/Api/SacolaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SacolaController extends Controller
+{
+    //
+}
---
+++ database/migrations/2023_03_02_024431_create_sacolas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sacolas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->decimal('total');
+            $table->foreignId('carrinho_id')->nullable(true)->constrained('carrinhos');
+            //$table->foreignId('loja_id')->nullable(false)->constrained('produtors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sacolas');
+    }
+};
