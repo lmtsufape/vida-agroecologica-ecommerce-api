d50184303b21671716a4d6e9dbb5512c9dca6db1
luiz-davi <luizd4398@gmail.com>
2023-03-13 22:13:24 -0300

    criando modelo + migração de bairro

---
+++ database/migrations/2023_03_14_005511_create_bairros_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bairros', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome')->unique();
+            $table->foreignId('cep_id')->nullable(false)->constrained('ceps');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bairros');
+    }
+};
