316082dfd5cb8ee6f747e9c6ddcf59e033fd23c6
Ivan do Nascimento <nascimentodoivan@gmail.com>
2023-03-14 14:43:15 -0300

    Melhorando a organização das rotas

--- routes/api.php
+++ routes/api.php
@@ -28,24 +28,33 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::apiResource('/produtor', ProdutorController::class)->except('store');
-
-    Route::apiResource('/consumidor', ConsumidorController::class)->except('store');
     Route::apiResource('/{userId}/endereco', EnderecoController::class);
-    Route::controller(ApiBancasController::class)->group(function () {
-        Route::post('bancas', 'store')->middleware(['check_produtor', 'check_bancas']);
-        Route::get('bancas', 'index');
-        Route::get('bancas/{id}', 'show');
-        Route::put('bancas/{banca}', 'update')->middleware('check_produtor');
-        Route::delete('bancas/{id}', 'destroy')->middleware(['check_produtor', 'check_valid_banca']);
+    //produtor
+    Route::middleware('check_produtor')->group(function () {
+        Route::apiResource('/produtor', ProdutorController::class)->except('store');
+
+        Route::controller(ApiBancasController::class)->group(function () {
+            Route::post('bancas', 'store')->middleware('check_bancas');
+            Route::get('bancas', 'index');
+            Route::get('bancas/{id}', 'show');
+            Route::put('bancas/{banca}', 'update');
+            Route::delete('bancas/{id}', 'destroy')->middleware('check_valid_banca');
+        });
+
+        Route::apiResource('banca/produto',ProdutoController::class);
+    });
+     //consumidor
+    Route::middleware('check_consumidor')->group(function () {
+        Route::apiResource('/consumidor', ConsumidorController::class)->except('store');
+
+        Route::apiResource('/sacolas', SacolaController::class)->only('index','destroy');
+        Route::post('/sacolas', [SacolaController::class, 'store'])->middleware('check_estoque');
     });
 
-    Route::apiResource('banca/produto',ProdutoController::class);
     Route::get('/categorias', function (Request $request) {
 
         return response()->json(['categorias'=> \App\Models\Categoria::all()]);
     });
-    Route::apiResource('/sacolas', SacolaController::class);
 });
 
 Route::post('/produtor', [ProdutorController::class, 'store']);
