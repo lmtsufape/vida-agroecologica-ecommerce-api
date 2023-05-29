c995702e94bd5c2cebc22161a828db5db6c363b2
Rener <rener.tome123@gmail.com>
2023-05-29 14:40:00 -0300

    Campo do formulário ajustado para ficar alinhado com os demais.

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -18,7 +18,7 @@
 
                 <input type="hidden" name="token" value="{{ $token }}">
 
-                <div class="mb-6">
+                <div class="mb-4">
                     <label for="email" class="block text-gray-700 font-bold mb-2">Email:</label>
                     <input id="email" type="text" name="email" required
                         class="w-full border border-gray-300 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
