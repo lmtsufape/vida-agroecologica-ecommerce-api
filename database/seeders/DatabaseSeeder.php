505ad9526653f95b657ad4ab29602e05ab68ef59
Rener <rener.tome123@gmail.com>
2023-07-24 16:17:57 -0300

    Removido seeder, controller e rota de endereço.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
     {
         DB::beginTransaction();
         $this->call([
-            EnderecoSeeder::class,
             ContatoSeeder::class,
             UserSeeder::class,
             AssociacaoSeeder::class,
