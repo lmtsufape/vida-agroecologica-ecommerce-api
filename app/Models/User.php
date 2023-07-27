f94d668867f39d875f042224a1b0fca7ab715b3b
Rener <rener.tome123@gmail.com>
2023-07-27 14:40:02 -0300

    Pequenas correções nas relações e tabelas.

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,7 +73,7 @@ class User extends Authenticatable implements MustVerifyEmail, reset
 
     public function roles()
     {
-        return $this->belongsToMany(Role::class);
+        return $this->belongsToMany(Role::class)->withTimestamps();
     }
 
     public function transacoes()
