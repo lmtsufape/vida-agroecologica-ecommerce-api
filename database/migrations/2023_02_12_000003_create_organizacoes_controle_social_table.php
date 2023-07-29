6fd488901a7957c393576bc2ea7cf1e1f70732cf
Rener <rener.tome123@gmail.com>
2023-07-28 22:07:10 -0300

    Pequena correção.

--- database/migrations/2023_02_12_000003_create_organizacoes_controle_social_table.php
+++ database/migrations/2023_02_12_000003_create_organizacoes_controle_social_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('organizacao_controle_socials');
+        Schema::dropIfExists('organizacoes_controle_social');
     }
 };
