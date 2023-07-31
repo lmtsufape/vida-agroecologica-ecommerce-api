00b76e249f8f527acc1cb9cc85cae7fbaa7da512
Rener <rener.tome123@gmail.com>
2023-07-31 19:26:58 -0300

    Validação de endereço removida do StoreUserRequest. Algumas correções.

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -13,17 +13,18 @@ class StoreUserRequest extends FormRequest
 
     public function rules()
     {
-        $rules = [
+        return [
             'name' => [
                 'required',
                 'regex:/^[a-zA-ZÀ-ÿ\s]+$/',  // regex para validar apenas letras do alfabeto (maiúsculas e minúsculas, com acento ou não) e espaços em branco.
                 'min:3',
-                'max:50'
+                'max:60'
             ],
             'email' => [
                 'required',
                 'string',
                 'email',
+                'max:60',
                 'unique:users'
             ],
             'password' => [
@@ -40,61 +41,52 @@ class StoreUserRequest extends FormRequest
             ],
             'telefone' => [
                 'required',
-                'regex:/^\(\d{2}\)\s\d{5}\-\d{4}$/', // considerando telefone no formato "(99) 99999-9999"
-                'unique:contatos'
+                'regex:/^\(\d{2}\)\s\d{5}\-\d{4}$/' // considerando telefone no formato "(99) 99999-9999"
             ],
             'cpf' => [
-                'nullable',
+                'required',
                 'regex:/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/', // considerando cpf no formato "999.999.999-99"
                 'unique:users'
-            ],
-            'rua' => [
-                'required',
-                'regex:/^[a-zA-ZÀ-ÿ\s]+$/',  // mesmo do "name"
-                'max:50'
-            ],
-            'cep' => [
-                'required',
-                'regex:/^\d{5}\-\d{3}$/' // considerando cep no formato "99999-999"
-            ],
-            'numero' => [
-                'required',
-                'integer',
-                'digits_between:1,4'
-            ],
-            'complemento' => [
-                'nullable',
-                'string',
-                'max:50'
-            ],
-            'cidade' => [
-                'required',
-                'regex:/^[a-zA-ZÀ-ÿ\s]+$/',  // mesmo do "name"
-                'max:50'
-            ],
-            'estado' => [
-                'required',
-                'regex:/^[a-zA-ZÀ-ÿ\s]+$/',  // mesmo do "name"
-                'max:50'
-            ],
-            'pais' => [
-                'required',
-                'regex:/^[a-zA-ZÀ-ÿ\s]+$/',  // mesmo do "name"
-                'max:50'
-            ],
-            'bairro_id' => [
-                'required',
-                'integer',
-                'max:5'
             ]
+            // 'rua' => [
+            //     'required',
+            //     'regex:/^[a-zA-ZÀ-ÿ\s]+$/',  // mesmo do "name"
+            //     'max:60'
+            // ],
+            // 'cep' => [
+            //     'required',
+            //     'regex:/^\d{5}\-\d{3}$/' // considerando cep no formato "99999-999"
+            // ],
+            // 'numero' => [
+            //     'required',
+            //     'integer',
+            //     'digits_between:1,4'
+            // ],
+            // 'complemento' => [
+            //     'nullable',
+            //     'string',
+            //     'max:50'
+            // ],
+            // 'cidade' => [
+            //     'required',
+            //     'regex:/^[a-zA-ZÀ-ÿ\s]+$/',  // mesmo do "name"
+            //     'max:60'
+            // ],
+            // 'estado' => [
+            //     'required',
+            //     'regex:/^[a-zA-ZÀ-ÿ\s]+$/',  // mesmo do "name"
+            //     'max:30'
+            // ],
+            // 'pais' => [
+            //     'required',
+            //     'regex:/^[a-zA-ZÀ-ÿ\s]+$/',  // mesmo do "name"
+            //     'max:30'
+            // ],
+            // 'bairro_id' => [
+            //     'required',
+            //     'integer',
+            // ]
         ];
-
-        if ($this->segment(2) === 'produtores') {
-            $rules['bairro_id'] = ['nullable', 'integer', 'max:5'];
-            $rules['bairro'] = ['required', 'regex:/^[a-zA-ZÀ-ÿ\s0-9]+$/', 'min:3', 'max:40'];
-        }
-
-        return $rules;
     }
 
     public function messages()
@@ -110,7 +102,7 @@ class StoreUserRequest extends FormRequest
             'email' => 'O email precisa ser válido.',
             'telefone.regex' => 'O campo telefone deve estar no formato (99) 99999-9999.',
             'cpf.regex' => 'O campo CPF deve estar no formato 999.999.999-99.',
-            'cep.regex' => 'O campo CEP deve estar no formato 99999-999.',
+            // 'cep.regex' => 'O campo CEP deve estar no formato 99999-999.',
         ];
     }
 }
