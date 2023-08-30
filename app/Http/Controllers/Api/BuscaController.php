a463bc98c613d600dffe8518ad482aa980739ae7
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2023-08-29 21:53:09 -0300

    Junção das funções de busca

--- app/Http/Controllers/Api/BuscaController.php
+++ app/Http/Controllers/Api/BuscaController.php
@@ -10,12 +10,7 @@ use Illuminate\Http\Request;
 
 class BuscaController extends Controller
 {
-    public function buscar_banca($nome)
-    {
-        $nomeBanca = Banca::where('nome', 'like', '%'.$nome.'%')->get();//pequisar independente do capslock
-
-        return response()->json(['nome' => $nomeBanca]);
-    }
+    
 
     public function buscar_produto(Request $request)
     {
@@ -27,15 +22,18 @@ class BuscaController extends Controller
 
         $tabelas = array();
 
+        $tabelas['nomeBanca'] = Banca::where('nome', 'ilike', '%'.$busca.'%')->get();//pequisar independente do capslock
+        $tabelas['nomeProdutor'] = User::where('name', 'ilike', '%'.$busca.'%')->whereHas('roles', function($query)
+        {
+            $query->where('nome', 'agricultor');
+        })->get();
+        $tabelas['produtos_categoria'] = ProdutoTabelado::where('categoria', 'ilike', '%'.$busca.'%')->get();
+
         $tabelas['produtos'] = ProdutoTabelado::where('nome', 'like', "%$busca%")
              ->join('produtos', 'produtos_tabelados.id', '=', 'produtos.produto_tabelado_id')
              ->select('produtos_tabelados.nome', 'produtos.*')
              ->get();
-        $tabelas['bancas'] = Banca::where('nome', 'like', "%$busca%")->get();
         $tabelas['categorias'] = ProdutoTabelado::where('categoria', 'like', "%$busca%")->distinct()->pluck('categoria');
-        // $tabelas['produtores'] = User::whereHas('user', function ($query) use ($busca) {
-        //     $query->where('name', 'like', "%$busca%");
-        // })->get();
 
         $tabelas = array_filter($tabelas, function ($valor)
         {
@@ -46,33 +44,6 @@ class BuscaController extends Controller
             return response()->json(['No Content' => 'Nenhum elemento encontrado.'], 204);
         }
         return response()->json($tabelas, 200);
-    }
-
-    public function buscar_vendedor($name)
-    {
-
-        $produtor = User::where('name', 'like', '%'.ucwords($name).'%')->whereHas('roles', function($query)
-        {
-            $query->where('nome', 'agricultor');
-        })->get();
-        if($produtor->count() == 0)
-        {
-            return response()->json(['erro' => "Nenhum produtor encontrado com o nome: $name"]);
-        }
-        return response()->json(['produtor' => $produtor]);
-
-    }
-
-    public function buscar_categoria(String $nomeCategoria)
-    {
-        $produtos = ProdutoTabelado::where('categoria', 'like', '%'.$nomeCategoria.'%')->get();
-
-        if ($produtos->count() == 0)
-        {
-            return response()->json(['erro' => "Nenhum produto encontrado em $nomeCategoria."], 400);
-        }
-
-        return response()->json(['produtos' => $produtos], 200);
     }
 
 
